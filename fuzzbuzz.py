362b72c01b26365d9b821863081c3860d731958a
colorido <anitamark2.0@gmail.com>
2015-09-17 20:34:24 +0900

    py

---
+++ fuzzbuzz.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Here your code !
+
+for num in range(1,100):
+    if num % 15 == 0:
+        print("fuzzbuzz")
+    elif num % 3 == 0:
+        print("fuzz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
