2bb8cf176371a4169f0b8837ad41a48872cfb990
rasbora <homingboost@gmail.com>
2015-09-18 10:03:33 +0900

    コードの整形

--- fuzzbuzz.py
+++ fuzzbuzz.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 
 for num in range(1,101):
-    if num % 15 == 0:
-        print("fuzzbuzz")
-    elif num % 3 == 0:
-        print("fuzz")
-    elif num % 5 == 0:
-        print("buzz")
+    if not num % 15:
+        print "fuzzbuzz"
+    elif not num % 3:
+        print "fuzz"
+    elif not num % 5:
+        print "buzz"
     else:
-        print(num)
+        print num
